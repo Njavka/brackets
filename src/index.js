ca357e811a2749cd24b26012f945587e7e0e72ca
Navka <navka@list.ru>
2024-08-31 14:42:57 +0300

    feat: brackets solution ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let length = str.length / 2;
+
+  for (let i = 0; i < length; i++) {
+    bracketsConfig.forEach((arr) => {
+      let pair = arr[0] + arr[1];
+      while (str.includes(pair)) {
+        str = str.replace(pair, "");
+      }
+    });
+  }
+  return !str.length;
+};
